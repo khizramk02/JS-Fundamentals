1901b905d546045d1091d24f1a2a359408291a82
Khizram Saeed Khan <43573930+khizramk02@users.noreply.github.com>
2020-07-18 20:12:36 +0500

    added js assignments

---
+++ ch # 03/app.js
@@ -0,0 +1,23 @@
+// Task # 01
+const age = 21;
+alert(`I am ${age} years old`);
+
+//Task # 02
+const visitor = 14;
+alert(`You have visited this site ${visitor} times.`);
+
+//Task # 03
+const birthYear = 1998;
+document.write(
+  `My birth year is ${birthYear} <br> Data type of my declared variable is ${typeof birthYear}`
+);
+
+//Task # 04
+const visitorName = 'John Doe';
+const productTitle = 'T-shirt';
+const quantity = 5;
+document.write('<br><br>');
+document.write(
+  `<span style="font-weight: bold;">${visitorName}</span> ordered <span style="font-weight: bold;">${quantity} 
+   ${productTitle}</span>(s) on XYZ Clothing store`
+);
